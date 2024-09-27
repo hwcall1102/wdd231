cc3b366328059ec42a4d851b1f3df3cd6fe1856e
hwcall1102 <haydenc0293@gmail.com>
2024-09-27 00:07:58 -0600

    toggle

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -7,13 +7,24 @@ const modified = document.querySelector("#lastModified");
 modified.innerHTML = `Last Modification <span>${document.lastModified}</span>`;
 
 // hamburger menu
-const mainnav = document.querySelector('.navigation')
+const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
 hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
+});
+
+const layoutSwitch = document.getElementById('layoutSwitch');
+const container = document.querySelector('.cards-grid');
+
+layoutSwitch.addEventListener('change', function() {
+  if (this.checked) {
+    container.classList.add('column');
+  } else {
+    container.classList.remove('column');
+  }
 });
 
 const cards = document.querySelector('#cards');
